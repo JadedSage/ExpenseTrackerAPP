d6ec50f4398691c5e1feab12d199a7b03af7b59d
JadedViper <jason.tedja@sjsu.edu>
2024-04-29 21:02:31 -0700

    Created Login Page

--- app/src/main/java/edu/sjsu/android/fpexpensetracker/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/fpexpensetracker/MainActivity.java
@@ -13,8 +13,7 @@ import edu.sjsu.android.fpexpensetracker.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    //TODO: Move attributes related to progressbar into its own class
-    private int CurrentProgress = 50; //Change for testing different lengths of progressbar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +21,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
 
-        binding.goalBar.setProgress(CurrentProgress);
-        binding.goalBar.setMax(100);
-
-
-
-
-
-        //TODO: See if below code matters
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
