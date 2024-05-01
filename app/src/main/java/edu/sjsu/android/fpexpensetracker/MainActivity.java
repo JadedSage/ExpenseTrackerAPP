29acd2b6a3e53e68ae61f4a410457cd7964fc691
JadedViper <jason.tedja@sjsu.edu>
2024-05-01 12:31:51 -0700

    Added Login Functionality to the program

--- app/src/main/java/edu/sjsu/android/fpexpensetracker/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/fpexpensetracker/MainActivity.java
@@ -1,18 +1,27 @@
 package edu.sjsu.android.fpexpensetracker;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import edu.sjsu.android.fpexpensetracker.databinding.ActivityMainBinding;
 
@@ -23,7 +32,15 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView mForgetPassword;
     private TextView mSignupHere;
+
+    private ProgressDialog mDialog;
+
+    private FirebaseAuth mAuth;
+
+    //TODO: We can prob remove this
     private ActivityMainBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
         loginDetails();
     }
     private void loginDetails(){
@@ -61,6 +81,23 @@ public class MainActivity extends AppCompatActivity {
                     mPass.setError("Password Required");
                     return;
                 }
+                mDialog.setMessage("Processing");
+                mDialog.show();
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            mDialog.dismiss();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                            Toast.makeText(getApplicationContext(), "Login Successful",Toast.LENGTH_SHORT);
+                        }
+                        else {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Login Failed",Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
             }
         });
         // Sends User to the Register Screen
